81ef4c375cd72a6d3ba930cfffd27ae2d600258b
PrajyotJagtap12 <167631303+PrajyotJagtap12@users.noreply.github.com>
2025-12-22 10:59:32 +0530

    changes

--- Frontend/babel.config.js
+++ Frontend/babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      {
+        unstable_transformProfile: 'default',
+      },
+    ],
+  ],
+  plugins: [],
+  overrides: [
+    {
+      test: /\.(js|jsx|ts|tsx)$/,
+      plugins: [
+        [
+          '@babel/plugin-transform-react-jsx',
+          {
+            runtime: 'classic',
+          },
+        ],
+      ],
+    },
+  ],
 };
